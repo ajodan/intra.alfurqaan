cff5eb661fa2acd9236cf10ef41b3b3a45a682bd
eksel <ekselcorp@gmail.com>
2023-01-13 22:30:06 +0700

    Perbaikan Konflik

--- resources/views/layouts/backend/sidebar.blade.php
+++ resources/views/layouts/backend/sidebar.blade.php
@@ -45,24 +45,7 @@
                 </li>
                 <li class="nav-item {{ Request::segment(1) == 'transaksi' ? '' : '' }}">
                     <a href="{{ route('laporan.index') }}" class="nav-link "><span class="pcoded-micon"><i class="feather icon-credit-card"></i></span><span class="pcoded-mtext">Laporan</span></a>
-                </li>
-<<<<<<< HEAD
-                <li class="nav-item {{ Request::is('bmm') ? 'active' : '' }} pcoded-menu-caption">
-                    <label>Menu Kas BMM</label>
-                </li>
-
-               
-                <li class="nav-item {{ Request::is('bendahara') ? 'active' : '' }} pcoded-menu-caption">
-                    <label>Menu Kas Masjid</label>
-                </li>
-
-=======
-                <li class="nav-item {{ Request::is('bendahara') ? 'active' : '' }} pcoded-menu-caption">
-                    <label>Menu Kas Masjid</label>
-                </li>
-
->>>>>>> 431a6504f8ccf8df1c7ef6d3f7d3c7864af47da3
-                <li class="nav-item {{ Request::segment(1) == 'kasmasjid' ? 'active' : '' }}">
+                </li>                <li class="nav-item {{ Request::segment(1) == 'kasmasjid' ? 'active' : '' }}">
                     <a href="{{ route('kasmasjid.index') }}" class="nav-link "><span class="pcoded-micon"><i class="feather icon-credit-card"></i></span><span class="pcoded-mtext">Transaksi</span></a>
                 </li>
                 <li class="nav-item {{ Request::segment(1) == 'jenistransaksi' ? 'active' : '' }}">
